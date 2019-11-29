2bea91bcdd3a9fd75974b43fd06a40986bc4cd7b
Vinny Murphy <vmurphy@cisco.com>
2019-11-29 09:43:51 -0500

    Add django_extensions and workouts

--- src/workout/models.py
+++ src/workout/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from django.db.models import (
+    CharField,
+    IntegerField,
+    Model,
+    SlugField,
+    TextField,
+)
 
-# Create your models here.
+
+class Workout(Model):
+    EXERCISE = (('S', 'Swim'), ('B', 'Bike'), ('R', 'Run'), ('O', 'Other'))
+    INTESITY = (
+        ('E', 'EASY'),
+        ('M', 'MODERATE'),
+        ('T', 'MODERATE2HIGH'),
+        ('H', 'HIGH'),
+    )
+    name = CharField(max_length=31)
+    slug = SlugField(max_length=31)
+    exercise = CharField(max_length=1, choices=EXERCISE)
+    duration = IntegerField(max_length=60)
+    intensity = CharField(max_length=1, choices=INTESITY)
+    description = TextField()
+
+
+class DailyWorkout(Model):
+    description = TextField(max_length=80)
